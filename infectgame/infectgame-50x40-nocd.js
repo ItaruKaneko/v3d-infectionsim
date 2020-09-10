75686d870c2d1240e0f96c264f27a60194dceeb3
ItaruKaneko <itaru-k@acm.org>
2020-09-10 14:24:40 +0900

    rev1.3 ppt based  on this

--- infectgame/infectgame-50x40-nocd.js
+++ infectgame/infectgame-50x40-nocd.js
@@ -2,28 +2,31 @@
 var c1;        // ゲームボード描画コンテキスト
 var c2;        // グラフ描画コンテキスト
 var copygame_agents;     // 全 copygame_agent を格納する配列
-var number_iga = 1600;    // number of infection game agents
 var number_of_creators; // copygame_agent の数
 var number_of_pirates; // pirates の数
 var gb;        // game bord, sixze 600
 var tick_count1;      // tick count 1
-var x_rr = new Array(20);
-var y_rr = new Array(20);
-var z_rr = new Array(4);
 
+var x_num = 50;
+var y_num = 40;
+var all_num = x_num * y_num * 4;
+var number_iga = all_num;    // number of infection game agents
+var x_rr = new Array(x_num);
+var y_rr = new Array(y_num);
+var z_rr = new Array(4);
 
 // infectgame agents 0.0
 // Previous version
 //    copygame04_04
 //     2019/11/21
-// x=20 x y=20 x z=4 の3次元格子
+// x=20 x y=y_num x z=4 の3次元格子
 // x は職場、yはグループ, z は家庭を表す
 // 再生産率は x, y, z 配列に
 // 
 
 // class definition : game_cell
 // ゲームのます目の定義
-function game_cell(n1,x1,y1,z1,x_rr1,y_rr1,z_rr1,st1,gb1) {
+function game_cell(n1,x1,y1,z1,x_rr1,y_rr1,z_rr1,st1,cd1,gb1) {
   this.n=n1; // cell number
   this.x=x1; // x coordinate 0..19 represents work place
   this.y=y1; // y coordinate 0..19 represents social group
@@ -32,8 +35,9 @@ function game_cell(n1,x1,y1,z1,x_rr1,y_rr1,z_rr1,st1,gb1) {
   this.y_rr=y_rr1; // reproduction rate for y coordinate 
   this.z_rr=z_rr1; // reproduction rate for x coordinate 
   this.st = st1; // status = 0
-  this.ci = 0+0 // capable to infect
+  this.cd = cd1; // contact detecter installed
   this.gb = gb1;  // game board itself
+  this.ci = 0+0 // capable to infect
 }
 
 game_cell.prototype.isinfected=function(){
@@ -66,52 +70,64 @@ game_cell.prototype.show=function(){
     } else {
       c1.fillStyle = 'rgb(128,255,128)'; // 緑 = 回復
     }
-    x2 = x1 * 20 + 6 - z1 * 2 + 10;
-    y2 = y1 * 16;
-    z2 = 9-z1 * 3;
-    c1.moveTo(x2,330-z2 - y2);
-    c1.lineTo(x2+12,330-z2 - y2);
-    c1.lineTo(x2+12,330-z2-4 - y2);
-    c1.lineTo(x2,330-z2-4 - y2);
-    c1.lineTo(x2,330-z2 - y2);
+    voff=y_num * 12 + 10;
+    x2 = x1 * 8 + 6 - z1 * 2 + 10;
+    y2 = y1 * 12;
+    z2 = 9-z1 * 2;
+
+    c1.moveTo(x2,voff-z2 - y2);
+    c1.lineTo(x2+4,voff-z2 - y2);
+    c1.lineTo(x2+4,voff-z2-4 - y2);
+    c1.lineTo(x2,voff-z2-4 - y2);
+    c1.lineTo(x2,voff-z2 - y2);
     c1.closePath();
     c1.fill();
 }
 
 game_cell.prototype.pick_xyz=function(x1,y1,z1) {
-  var gb1=this.gb[z1 * 400 + y1 * 20 + x1];
+  var gb1=this.gb[(z1 * y_num + y1) * x_num + x1];
   return(gb1);
+}
+
+game_cell.prototype.meet=function(gb1) {
+  ci1 = gb1.ci;
+  if (gb1.ci>0) {
+    if (this.cd>0 && gb1.cd>0) {
+      // both has contact detector
+      this.cd = 2; // contact detected so far the person will be isolated
+        // and does not infect to others
+      this.ci = 0;
+      // so far the person is infected but will not infect others
+      this.st = 1;
+    }
+    else {
+      // contact is not detected and person may infect others
+      this.st = 1;
+      this.ci = 1;
+    }
+  }
 }
 
 game_cell.prototype.day=function(){
   if (this.st == 0) {  // not infected
+    // 感染していないエージェント
     var x1 = this.x; // x community index
     var y1 = this.y; // y family index
     var z1 = this.z;
     // meet some one else in different x
     r1 = Math.random();                   // infection ratio
-    for(var r2=0; r2<20; r2++) {
+    for(var r2=0; r2<y_num; r2++) {
     //r2 = Math.floor(Math.random() * 20);  // other person to meet
       if (r1 < this.x_rr[this.x]) {
-        gb1 = this.pick_xyz(x1,r2,z1);
-        ci1 = gb1.ci;
-        if (gb1.ci>0) {
-          this.st = 1;
-          this.ci = 1;
-        }
+        this.meet(this.pick_xyz(x1,r2,z1));
       }
     }
     // meet some one else in different y
     r1 = Math.random();                   // infection ratio
-    for(var r2=0; r2<20; r2++) {
+    for(var r2=0; r2<x_num; r2++) {
     //r2 = Math.floor(Math.random() * 20);  // other person to meet
       if (r1 < this.y_rr[this.y]) {
-        gb1 = this.pick_xyz(r2,y1,z1);
-        ci1 = gb1.ci;
-        if (gb1.ci>0) {
-          this.st = 1;
-          this.ci = 1;
-        }
+        this.meet(this.pick_xyz(r2,y1,z1));
       }
     }
     // meet some one else in different z
@@ -119,23 +135,21 @@ game_cell.prototype.day=function(){
     for(var r2=0; r2<4; r2++) {
     // r2 = Math.floor(Math.random() * 4);  // other person to meet
       if (r1 < this.z_rr[this.z]) {
-        gb1 = this.pick_xyz(x1,y1,r2);
-        ci1 = gb1.ci;
-        if (gb1.ci>0) {
-          this.st = 1;
-          this.ci = 1;
-        }
+        this.meet(this.pick_xyz(x1,y1,r2));
       }
     }
   }
   else {
-    // st 1..4 hiding
-    // st 8..9 symptic
-    // st 10 detectable
-    // st = 12 recovered
+    // 感染しているエージェント
+    // st 1..4 hiding 非発症
+    // st 8..9 symptic 発症中/未検出
+    // st 10 detectable 発症中/検出可能
+    // st = 12 recovered 回復(感染しない)
     if (this.st <5 ) { // Hiding
+      // 非発症
     }
-    else if (this.st <9 ) { // infectable
+    else if (this.st <12 ) { // infectable
+      // st=5..11 発症中, 検出可能
       this.ci = 1;
     }
     else if (this.st == 12) { // end of infetable
@@ -156,6 +170,8 @@ game_cell.prototype.infect=function() {
   this.st = 1;
 }
 
+
+
 // 現状をplotする
 
 function plot_status(){
@@ -171,7 +187,7 @@ function plot_status(){
     }
   }
   c2.beginPath();
-  c2.rect(x1*2 + 40,390-esum*2 - 1, 2,2);
+  c2.rect(x1 + 40,390-esum - 1, 2,2);
   c2.fillStyle = 'rgb(0,64,64)'; // 紺色
   c2.fill();
 }
@@ -182,16 +198,16 @@ function plot_axis(){
   c2.rect(40,10,360,380);
   c2.stroke();
   var n;
-  for (n=0; n<190; n+=20) {
+  for (n=0; n<361; n+=20) {
     c2.beginPath();
     c2.fillStyle = 'rgb(0,0,0)'; // 紺色
     c2.rect(n*2+40,386,1,4);
     c2.fill();
     c2.font = "10px 'ＭＳ Ｐゴシック'";
     c2.strokeStyle = "blue";
-    c2.fillText(n.toString(10),n*2+35,402);
+    c2.fillText(n.toString(10),n+35,402);
   }
-  for (n=0; n<190; n+=20) {
+  for (n=0; n<360; n+=20) {
     // 左側の目盛
     c2.beginPath();
     c2.fillStyle = 'rgb(0,0,0)'; // 黒
@@ -199,7 +215,7 @@ function plot_axis(){
     c2.fill();
     c2.font = "10px 'ＭＳ Ｐゴシック'";
     c2.strokeStyle = "blue";
-    c2.fillText(n.toString(10),20,390 - n*2);
+    c2.fillText(n.toString(10),20,390 - n);
   }
   //c2.filltext("tick",210,220)
 }
@@ -207,14 +223,15 @@ function plot_axis(){
 // copygame_agent クラスを使ったアニメーションの本体
 // 毎秒 30 回実行する関数
 function tick1() {
-  if (tick_count1 > 180) {return; }
+  // 359 回目で終了
+  if (tick_count1 > 358) {return; }
   tick_count1=tick_count1+1;
   // 描画領域をいったんクリアする
   c1.clearRect(0, 0, 600, 600);
 
-  // 20個の円についてのループ
+  // 全エージェント(all_num個)についての処理ループ
   var n;
-  for (n = 0; n < 1600; n++){
+  for (n = 0; n < all_num; n++){
     gb[n].day();
     gb[n].show();
   }
@@ -236,17 +253,23 @@ function draw_canvas() {
   }
   tick_count1=0; // tick count をゼロリセット
   // initializatio of the board
-  gb = new Array(1600);
-  for (var x1=0; x1<20; x1++) { x_rr[x1]=0.0050; }
-  for (var y1=0; y1<20; y1++) { y_rr[y1]=0.0044; }
-  for (var z1=0; z1<4; z1++) { z_rr[z1]=0.0006; }
+  gb = new Array(all_num);
+  for (var x1=0; x1<5; x1++) { x_rr[x1]=0.425/x_num/8; } // 10 % of group
+  for (var y1=0; y1<4; y1++) { y_rr[y1]=0.425/y_num/8; } // 10 % of group
+  for (var x1=5; x1<x_num; x1++) { x_rr[x1]=0.425/x_num/8; } // 90 % of group
+  for (var y1=4; y1<y_num; y1++) { y_rr[y1]=0.425/y_num/8; } // 90 % of group
+  for (var z1=0; z1<4; z1++) { z_rr[z1]=0.25/4/8; }
   // game bord のクリア
   var n1=0;
   for (var z1=0; z1<4; z1++) {
-    for (var y1=0; y1<20; y1++) {
-      for (var x1=0; x1<20; x1++) {
-         gb[n1]=new game_cell(n1,x1,y1,z1,x_rr,y_rr,z_rr,0,gb);
-         n1++;
+    for (var y1=0; y1<y_num; y1++) {
+      for (var x1=0; x1<x_num; x1++) {
+        cd1 = 0; // nocd
+        //var cd1 = Math.floor(1 + Math.random() - 0.3); // contact detector 70%
+        //if (x1<5) { cd1 = 0;}
+        //if (y1<4) { cd1 = 0;}
+        gb[n1]=new game_cell(n1,x1,y1,z1,x_rr,y_rr,z_rr,0,cd1,gb);
+        n1++;
        }
     }
   }
